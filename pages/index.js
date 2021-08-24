5ef4ce797a8dc94ec3a4ce48a6d65a2736cf2a13
winhtoomyint <winhtoomyint@gmail.com>
2021-08-25 05:39:39 +0630

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return(
+        <div>
+            <img src="https://image.freepik.com/free-vector/hello-word-memphis-background_136321-401.jpg" />
+        </div>
+    )
+        
+    
+}
+
+export default Home;
